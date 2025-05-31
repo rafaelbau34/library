8cb9bdc7148263077b1186f002538c5886f81f7b
rafaelbau34 <rafaelbaugar@gmail.com>
2025-05-31 00:57:17 -0600

    some progress with the js functions

--- main.js
+++ main.js
@@ -5,15 +5,25 @@ function Book(author, title, pages, read) {
   this.read = read;
 }
 
-const book1 = new Book("George Orwell", "1984", "340", false);
-
-function addBookToLibrary() {}
-
-function showForm() {}
+const myLibrary = [];
+function addBookToLibrary() {
+  const list = document.getElementById("book-list");
+  list.innerHTML = "";
+  myLibrary.forEach((book, index) => {
+    const div = document.createElement("div");
+    div.textContent = `${book.title} by ${book.author}, ${book.pages} pages, Read: ${bbook.read}`;
+    list.appendChild(div);
+  });
+}
 
 const popUpForm = document.getElementById("popUpForm");
 const button = document.getElementById("addBook");
+const closeButton = document.getElementById("closeBtn");
 
 button.addEventListener("click", function () {
   document.getElementById("popUpForm").style.display = "flex";
+});
+
+closeButton.addEventListener("click", function () {
+  document.getElementById("popUpForm").style.display = "none";
 });
