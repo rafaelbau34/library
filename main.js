e7dd0267d39329e9120d873812fdb0d4205e0ad4
rafaelbau34 <rafaelbaugar@gmail.com>
2025-05-31 01:58:03 -0600

    now it adds the objects to the array and displays them, but it needs heavy css

--- main.js
+++ main.js
@@ -6,12 +6,13 @@ function Book(author, title, pages, read) {
 }
 
 const myLibrary = [];
+
 function addBookToLibrary() {
   const list = document.getElementById("book-list");
   list.innerHTML = "";
   myLibrary.forEach((book, index) => {
     const div = document.createElement("div");
-    div.textContent = `${book.title} by ${book.author}, ${book.pages} pages, Read: ${bbook.read}`;
+    div.textContent = `${book.title} by ${book.author}, ${book.pages} pages, Read: ${book.read}`;
     list.appendChild(div);
   });
 }
@@ -26,4 +27,19 @@ button.addEventListener("click", function () {
 
 closeButton.addEventListener("click", function () {
   document.getElementById("popUpForm").style.display = "none";
+});
+
+document.getElementById("bookForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const author = e.target.author.value;
+  const title = e.target.title.value;
+  const pages = e.target.pages.value;
+  const read = e.target.option.value;
+
+  const newBook = new Book(author, title, pages, read);
+  myLibrary.push(newBook);
+  addBookToLibrary();
+
+  e.target.reset();
 });
